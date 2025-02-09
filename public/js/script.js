67b7bb59bcaaaceeab4de36198cccceedc94ac3e
nzmylt <glmzhn.daulet@gmail.com>
2025-02-09 18:41:01 +0500

    Updated project4

--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,20 @@
 const answers_no = {
     english: [
         "No",
-        "Are you sure?",
-        "Are you really sure??",
-        "Are you really realy sure???",
-        "Think again?",
-        "Don't believe in second chances?",
-        "Why are you being so cold?",
-        "Maybe we can talk about it?",
-        "I am not going to ask again!",
-        "Ok now this is hurting my feelings!",
-        "You are now just being mean!",
-        "Why are you doing this to me?",
-        "Please give me a chance!",
-        "I am begging you to stop!",
-        "Ok, Let's just start over.."
+        "No",
+        "No",
+        "No",
+        "No",
+        "No",
+        "No",
+        "No",
+        "No",
+        "No",
+        "No",
+        "No",
+        "No",
+        "No",
+        "Ok, Let's just start over."
     ],
     french: [
         "Non",
@@ -69,7 +69,7 @@ no_button.addEventListener('click', () => {
     // Change banner source
     let banner = document.getElementById('banner');
     if (clicks === 0) {
-        banner.src = "public/images/yapa.jpg";
+        banner.src = "public/images/me.jpg";
         refreshBanner();
     }
     clicks++;
